fa38f5da564a6a060d20de1427d72a79f5a5df2a
Eduardo <eduardo_reolon@hotmail.com>
2026-03-16 16:11:09 -0300

    configuracao server

--- core/urls.py
+++ core/urls.py
@@ -35,4 +35,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('ler-nota/', ler_nota, name='ler_nota'),
     path('sw.js', TemplateView.as_view(template_name='sw.js', content_type='application/javascript')),
+    path('manifest.json', TemplateView.as_view(template_name='manifest.json', content_type='application/json')),
 ]
