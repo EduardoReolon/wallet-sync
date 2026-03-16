e68ba85a2ec7ac1d3e37af6f0f6d9e16a077bfd0
Eduardo <eduardo_reolon@hotmail.com>
2026-03-16 15:15:51 -0300

    arquivos estaticos

--- core/settings.py
+++ core/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'assets/'
-STATIC_ROOT = '/home/ubuntu/consultoria-ia/public'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Pasta gerada no servidor
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"), # ou o nome da sua pasta de assets local
+    os.path.join(BASE_DIR, 'static'), # Pasta onde você cria os arquivos
 ]
